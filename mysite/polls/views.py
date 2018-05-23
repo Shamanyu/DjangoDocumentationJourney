bd0ae73ff4e9b9b53e6d8f1d564a4ebde4bee7b6
shamanyu <shubhamshamanyu@gmail.com>
2018-05-23 17:57:16 +0530

    Implement endpoint 'vote' to enable users to vote on questions

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
 # Write views
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 """
 get_object_or_404 either returns an object or raises Http404, passing the
 optional keyword arguments to 'get' method.
@@ -8,6 +8,7 @@ get_list_or_404 either returns a list or raises Http404, passing the
 optional keyword arguments to 'filter' method.
 """
 from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.urls import reverse
 
 from .models import Question
 
@@ -36,4 +37,21 @@ def results(request, question_id):
 
 def vote(request, question_id):
 
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+    """ Always return an HttpResponseRedirect after successfully dealing with
+    POST data. This prevents data from being posted twice if a user hits the
+    'back' button. Moreover, use 'reverse' to build redirect url to prevent
+    hardcoding
+    """
+    return HttpResponseRedirect(reverse('polls:results', args=(question_id, )))
