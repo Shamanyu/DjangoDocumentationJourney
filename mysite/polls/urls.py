ababa319f5fa8b749f920b5876e6a806b1de9b58
shamanyu <shubhamshamanyu@gmail.com>
2018-05-23 15:24:44 +0530

    Add placeholder views for 'Question' model

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,5 +7,9 @@ allows us to make global changes to the URL patterns of our project while
 only changing a single file
 """
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # '<>' helps capture the variable and pass it off as parameter to the view
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='detail'),
 ]
