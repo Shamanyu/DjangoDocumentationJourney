bdd56e6735d3bfd67800bcfd80e5321b1f0fe58f
shamanyu <shubhamshamanyu@gmail.com>
2018-05-23 17:23:27 +0530

    Bugfix: 'vote' view was named 'detail'

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     # '<>' helps capture the variable and pass it off as parameter to the view
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
-    path('<int:question_id>/vote/', views.vote, name='detail'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
