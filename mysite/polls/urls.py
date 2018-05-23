e49cf7706495b488b98fb07934fe6bc07ae122c1
shamanyu <shubhamshamanyu@gmail.com>
2018-05-23 10:32:16 +0530

    Bugfix: 'Polls' app unable to be hosted due to syntax issue

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from . import views
 
+""" The 'name' parameter is used when referencing a URL from templates. This
+allows us to make global changes to the URL patterns of our project while
+only changing a single file
+"""
 urlpatterns = [
-    """ The 'name' parameter is used when referencing a URL from templates. This
-    allows us to make global changes to the URL patterns of our project while
-    only changing a single file"""
     path('', views.index, name='index')
 ]
