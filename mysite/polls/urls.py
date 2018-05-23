28b8a5f94d972fcddad527e040cf436c522acf2b
shamanyu <shubhamshamanyu@gmail.com>
2018-05-23 10:24:17 +0530

    Add further comments to help user understand the code for the first written view

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    """ The 'name' parameter is used when referencing a URL from templates. This
+    allows us to make global changes to the URL patterns of our project while
+    only changing a single file"""
     path('', views.index, name='index')
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+# The first argument is necessarily a HttpRequest object
 def index(request):
 
     return HttpResponse("Hey there, you're at the polls index!")
