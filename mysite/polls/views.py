284c3f13bbe82951959df0f7bfb5650afae0d1d5
shamanyu <shubhamshamanyu@gmail.com>
2018-05-23 10:20:33 +0530

    Write the first view, exposing the '/' endpoint of polls app

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,9 @@
 # Write views
 
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+
+    return HttpResponse("Hey there, you're at the polls index!")
