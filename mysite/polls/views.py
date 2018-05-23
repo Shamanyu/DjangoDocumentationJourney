c5dcb95b97438164e2f6b2f863588130060532a6
shamanyu <shubhamshamanyu@gmail.com>
2018-05-23 17:06:02 +0530

    Use 'get_object_or_404' shortcut to make 'detail' view less verbose

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
 # Write views
 
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -19,10 +19,7 @@ def index(request):
 
 def detail(request, question_id):
 
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
 
     return render(request, 'polls/detail.html', {'question': question})
 
