24636969d95d641cbe94323379c115438760886c
shamanyu <shubhamshamanyu@gmail.com>
2018-05-24 06:22:07 +0530

    Add test case in 'polls' app to verify if 'was_published_recently' method works correctly

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,5 +1,22 @@
 # Write test cases
 
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
 
 # Create your tests here.
+
+"""
+python manage.py test polls creates a special database for testing. It then
+looks for test methods whose name begin with 'test'.
+"""
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
